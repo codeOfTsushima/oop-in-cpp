b052cf7e45687b24e7641d7b6402191ab86ff2c7
codeOfTsushima <parthip.mp303@gmail.com>
2026-02-24 22:35:52 +0530

    Updated classes and added userdefined functions

--- class.cpp
+++ class.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 //Classes are userdefined data types
+
 class Employee{
+    public:
     string Name;
     string Company;
     int Age;
    
+    void Introduction(){
+        cout<<"Name - "<<Name<<endl;
+        cout<<"Company - "<<Company<<endl;
+        cout<<"Age - "<<Age<<endl;
+        
+        
+    }
 };
 int main()
 {
 
     Employee emp1; //variable of type employee
-    
+    emp1.Name="Parthip";
+    emp1.Company="Google";  
+    emp1.Age=19;
+    emp1.Introduction();
+
 }
 //There are 3 access specifers in Cpp. They are private, public and protected
+//Everyting inside the class is private by default
