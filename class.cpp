5f9aebc6f49de7cd26109d1b4719f54430e7157e
codeOfTsushima <parthip.mp303@gmail.com>
2026-02-24 22:15:01 +0530

    Added Classes in OOPs

---
+++ class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Classes are userdefined data types
+class Employee{
+    string Name;
+    string Company;
+    int Age;
+   
+};
+int main()
+{
+
+    Employee emp1; //variable of type employee
+    
+}
+//There are 3 access specifers in Cpp. They are private, public and protected
