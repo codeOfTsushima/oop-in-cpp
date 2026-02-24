a9a7e66addf2e6780efd3d157be76f8b40ab3351
codeOfTsushima <parthip.mp303@gmail.com>
2026-02-24 22:42:04 +0530

    Added another class

--- class.cpp
+++ class.cpp
@@ -8,7 +8,7 @@ class Employee{
     string Company;
     int Age;
    
-    void Introduction(){
+    void IntroduceYourself(){
         cout<<"Name - "<<Name<<endl;
         cout<<"Company - "<<Company<<endl;
         cout<<"Age - "<<Age<<endl;
@@ -23,7 +23,13 @@ int main()
     emp1.Name="Parthip";
     emp1.Company="Google";  
     emp1.Age=19;
-    emp1.Introduction();
+    emp1.IntroduceYourself();
+
+   Employee emp2;
+   emp2.Name="GiggaChad";
+   emp2.Company="Bigbazaar";
+   emp2.Age=67;
+   emp2.IntroduceYourself();
 
 }
 //There are 3 access specifers in Cpp. They are private, public and protected
