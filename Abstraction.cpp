0190699f0453cf65b2d1f824d34487b8d26fbc78
codeOfTsushima <parthip.mp303@gmail.com>
2026-03-17 23:09:25 +0530

    Added Abstraction

---
+++ Abstraction.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+
+class AbstractEmployee{ //this is an abstract function 
+   virtual void askforpromotion()=0;
+
+};
+class Employee:AbstractEmployee{
+private:
+    string Name;
+    string Company;
+    int Age;
+//Encapsulation
+public:
+
+    void setName(string name){ //setter
+        Name = name;
+
+    }
+    string getName(){ //getter
+        return Name;
+    }
+    void setCompany(string company){
+        Company=company;
+    }
+    string getCompany(){
+        return Company;
+    }
+    void setAge(int age){
+        if(age>=18)
+        Age = age;
+    }
+    int getAge(){
+        return Age;
+    }
+
+    void IntroduceYourself(){  
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+     }
+    Employee(string Name,string Company,int age){
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+    }
+    void askforpromotion(){
+        if(Age>30)
+        cout<<Name<<"Got promoted!"<<endl;
+        else
+        cout<<"Sorry no promotion for you"<<endl;
+
+    }
+};
+
+int main(){
+    Employee employee1 = Employee("Parthip", "Amazon", 34);
+    employee1.IntroduceYourself();
+
+    Employee employee2 = Employee("Neymar", "Football", 34);
+    employee2.IntroduceYourself();
+
+    employee1.setAge(15);
+    cout<<employee1.getName()<< " is "<<employee1.getAge()<<" years old"<<endl;
+
+    employee1.askforpromotion();
+    employee2.askforpromotion();
+}
