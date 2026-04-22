d86e139bc8dd65cc8ea1d2ee26cfc89ec38dfc88
codeOfTsushima <parthip.mp303@gmail.com>
2026-04-22 23:57:37 +0530

    Added Leetcode 14, Lonmgest common prefix

---
+++ Leetcode/L14_longestCommonPrefix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+        for(int i = 0; i<strs[0].size();i++){
+            for(auto s:strs){
+                if(s[i]!=strs[0][i]){
+                    return res;
+                }
+            }
+            res += strs[0][i];
+        }
+        return res;   
+    }
+};
+int main(){
+    
+}
